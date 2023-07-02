2a52af3a63bbaa2cad5246b1c0df5d5c93daf22a
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-02 09:21:43 +0300

    Create Category Model

---
+++ BulkyStoreWeb/Models/Cataegory.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyStoreWeb.Models
+{
+    public class Cataegory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
