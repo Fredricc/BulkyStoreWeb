f79966698c6b644f9d2b152c4a01085587f88f0a
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-02 09:40:14 +0300

    Add Connection String

--- BulkyStoreWeb/Models/Cataegory.cs
+++ BulkyStoreWeb/Models/Cataegory.cs
@@ -1,9 +1,12 @@
-﻿namespace BulkyStoreWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyStoreWeb.Models
 {
     public class Cataegory
     {
-        []
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
