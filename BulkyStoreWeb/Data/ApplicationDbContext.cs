b04474f4492c1fada1ff52ae80024e4741a408ec
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-02 11:06:05 +0300

    Add ApplicationDbContext

---
+++ BulkyStoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyStoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category>
+    }
+}
--- BulkyStoreWeb/Models/Category.cs
+++ BulkyStoreWeb/Models/Category.cs
@@ -2,7 +2,7 @@
 
 namespace BulkyStoreWeb.Models
 {
-    public class Cataegory
+    public class Category
     {
         [Key]
         public int Id { get; set; }
