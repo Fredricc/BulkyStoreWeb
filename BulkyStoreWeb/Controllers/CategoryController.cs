4c2286c7193bc7e56ee8794f3989e63c48a2dace
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-10 20:02:46 +0300

    Delete Category

--- BulkyStoreWeb/Controllers/CategoryController.cs
+++ BulkyStoreWeb/Controllers/CategoryController.cs
@@ -103,22 +103,19 @@ namespace BulkyStoreWeb.Controllers
         }
 
         //POST
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(Category obj)
+        public IActionResult DeletePOST(int? id)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
+            var obj = _db.Categories.Find(id);
+            if(obj == null)
             {
-                ModelState.AddModelError("name", "The  DisplayOrder cannot exacly match the Name.");
+                return NotFound();
             }
-            if (ModelState.IsValid)
-            {
                 _db.Categories.Remove(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
-            }
 
-            return View(obj);
         }
     }
 }
