79a7fd9dd1bb0f0197630eaa87a9b1b644ca4318
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-07-25 17:08:23 +0300

    .Net8

--- BulkyStoreWeb/Controllers/HomeController.cs
+++ BulkyStoreWeb/Controllers/HomeController.cs
@@ -1,21 +1,37 @@
 ﻿using BulkyStore.Models;
+using BulkyStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace BulkyStoreWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            this._unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category, CoverType");
+            return View(productList);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category, CoverType");
+            };
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
